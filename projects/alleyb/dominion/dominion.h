347a547a4479e2043fc264531d7089f7d9e52dd9
Ben Alley <balley@sqasquared.com>
2019-04-19 17:53:55 -0700

    add smithy and adventurer

--- projects/alleyb/dominion/dominion.h
+++ projects/alleyb/dominion/dominion.h
@@ -81,11 +81,19 @@ struct gameState* newGame();
 int* kingdomCards(int k1, int k2, int k3, int k4, int k5, int k6, int k7,
 		  int k8, int k9, int k10);
 
+// new card effect functions /////////////////////////////////////////
+int smithyEffect(int currentPlayer, struct gameState *state, int handPos);
+
+int adventurerEffect(int currentPlayer, struct gameState *state);
+
+
+////////////////////////////////////////////////////////////////////
+
 int initializeGame(int numPlayers, int kingdomCards[10], int randomSeed,
 		   struct gameState *state);
 /* Responsible for initializing all supplies, and shuffling deck and
    drawing starting hands for all players.  Check that 10 cards selected
-   are in fact (different) kingdom cards, and that numPlayers is valid. 
+   are in fact (different) kingdom cards, and that numPlayers is valid.
 
 Cards not in game should initialize supply position to -1 */
 
